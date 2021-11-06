ef175a88be14f7e37d72c6b0a1d32891863046fa
Johannes Siedersberger <johannes.siedersberger@gmail.com>
2021-11-06 11:18:04 +0100

    Create Spring Project

---
+++ spring-crm-backend/src/main/java/com/jsitconsulting/springcrmbackend/SpringCrmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jsitconsulting.springcrmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrmBackendApplication.class, args);
+	}
+
+}
