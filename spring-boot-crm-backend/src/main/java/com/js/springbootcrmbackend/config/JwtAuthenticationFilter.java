ea91326fe40245af0619db201bf49e3e65c35c42
Johannes Siedersberger <johannes.siedersberger@gmail.com>
2023-07-06 22:58:43 +0200

    Fix Typo in Bearer

--- spring-boot-crm-backend/src/main/java/com/js/springbootcrmbackend/config/JwtAuthenticationFilter.java
+++ spring-boot-crm-backend/src/main/java/com/js/springbootcrmbackend/config/JwtAuthenticationFilter.java
@@ -35,7 +35,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         final String authHeader = request.getHeader("Authorization");
 
-        if (authHeader == null || !authHeader.startsWith("Baerer ")) {
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
