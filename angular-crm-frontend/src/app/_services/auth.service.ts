02552080bad3e48b0926773a5f6357a78b63e0e9
Johannes Siedersberger <johannes.siedersberger@gmail.com>
2023-08-08 20:44:59 +0200

    created auth service, token-service

---
+++ angular-crm-frontend/src/app/_services/auth.service.ts
@@ -0,0 +1,40 @@
+import { EventEmitter, Output, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { LocalStorageService } from 'ngx-webstorage';
+import { LoginRequestPayload } from '../login-page/login-request.payload';
+import { map, tap } from 'rxjs/operators';
+import { LoginResponse } from '../login-page/login-response.payload';
+
+const AUTH_API = 'http://localhost:8080/api/auth/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(AUTH_API + 'authenticate', {
+      email,
+      password
+    }, httpOptions);
+  }
+
+  register(email: string, password: string, password2: string): Observable<any> {
+    return this.http.post(AUTH_API + 'signup', {
+      email,
+      password,
+      password2
+    }, httpOptions);
+  }
+
+
+
+}
