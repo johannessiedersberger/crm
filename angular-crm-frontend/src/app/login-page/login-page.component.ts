257bc3ab1511f847381bf7187989ad15db6fc3c1
Johannes Siedersberger <johannes.siedersberger@gmail.com>
2023-08-08 20:48:36 +0200

    implemented login logic

--- angular-crm-frontend/src/app/login-page/login-page.component.ts
+++ angular-crm-frontend/src/app/login-page/login-page.component.ts
@@ -1,17 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginRequestPayload } from './login-request.payload';
+import { ToastrService } from 'ngx-toastr';
+import { throwError } from 'rxjs';
+import { TokenStorageService } from '../_services/token-storage.service';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
+  providers: [MessageService]
 })
-export class LoginPageComponent {
-  username: string;
-  password: string;
+export class LoginPageComponent implements OnInit {
 
-  constructor() {
-    this.username = "";
-    this.password = "";
+  form: any = {
+    username: null,
+    password: null
+  };
+  isLoggedIn = false;
+  isLoginFailed = false;
+  loginSuccessFull = false;
+  errorMessage = '';
+  roles: string[] = [];
+
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService, private router: Router, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+      this.roles = this.tokenStorage.getUser().roles;
+    }
+  }
+
+  login(): void {
+    const { username, password } = this.form;
+
+    this.authService.login(username, password).subscribe({
+      next: data => {
+        console.log(data)
+        this.tokenStorage.saveToken(data.access_token);
+        this.tokenStorage.saveUser(data);
+
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.loginSuccessFull = true;
+        this.roles = this.tokenStorage.getUser().roles;
+        this.router.navigateByUrl('dashboard');
+      },
+      error: err => {
+        console.log(err);
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+        this.showError(this.errorMessage);
+      }
+    });
+  }
+
+  showError(err: any) {
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: err });
   }
 
 }
